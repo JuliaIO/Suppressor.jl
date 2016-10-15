f5646a7ab0ad9cd9bcf1c3bfddb9aaa44b62232e
Spencer Russell <spencer.f.russell@gmail.com>
2016-10-15 10:59:27 -0400

    Add capture* macros and improve suppress* ones. (#3)
    * adds capture_out and capture_err macros
    
    * updates the README with capture_ macro info
    
    * updates test so it passes with color enabled
    
    * updates appveyor config to enable color, like travis
    
    * now closes the write end of redirect streams
    
    I think if you don't close the streams then the reader task will
    never finish. I also removed some assignments that didn't seem to
    be used anywhere.

--- src/Suppressor.jl
+++ src/Suppressor.jl
@@ -3,6 +3,7 @@ __precompile__()
 module Suppressor
 
 export @suppress, @suppress_out, @suppress_err
+export @capture_out, @capture_err
 
 macro suppress(block)
     quote
@@ -17,13 +18,11 @@ macro suppress(block)
 
             value = $(esc(block))
 
-            @async wait(out_reader)
-            REDIRECTED_STDOUT = STDOUT
-            out_stream =redirect_stdout(ORIGINAL_STDOUT)
+            redirect_stdout(ORIGINAL_STDOUT)
+            close(out_wr)
 
-            @async wait(err_reader)
-            REDIRECTED_STDERR = STDERR
-            err_stream = redirect_stderr(ORIGINAL_STDERR)
+            redirect_stderr(ORIGINAL_STDERR)
+            close(err_wr)
 
             return value
         end
@@ -39,9 +38,8 @@ macro suppress_out(block)
 
             value = $(esc(block))
 
-            @async wait(out_reader)
-            REDIRECTED_STDOUT = STDOUT
-            out_stream = redirect_stdout(ORIGINAL_STDOUT)
+            redirect_stdout(ORIGINAL_STDOUT)
+            close(out_wr)
 
             return value
         end
@@ -57,11 +55,44 @@ macro suppress_err(block)
 
             value = $(esc(block))
 
-            @async wait(err_reader)
-            REDIRECTED_STDERR = STDERR
-            err_stream = redirect_stderr(ORIGINAL_STDERR)
+            redirect_stderr(ORIGINAL_STDERR)
+            close(err_wr)
 
             return value
+        end
+    end
+end
+
+macro capture_out(block)
+    quote
+        if ccall(:jl_generating_output, Cint, ()) == 0
+            ORIGINAL_STDOUT = STDOUT
+            out_rd, out_wr = redirect_stdout()
+            out_reader = @async readstring(out_rd)
+
+            $(esc(block))
+
+            redirect_stdout(ORIGINAL_STDOUT)
+            close(out_wr)
+
+            wait(out_reader)
+        end
+    end
+end
+
+macro capture_err(block)
+    quote
+        if ccall(:jl_generating_output, Cint, ()) == 0
+            ORIGINAL_STDERR = STDERR
+            err_rd, err_wr = redirect_stderr()
+            err_reader = @async readstring(err_rd)
+
+            $(esc(block))
+
+            redirect_stderr(ORIGINAL_STDERR)
+            close(err_wr)
+
+            wait(err_reader)
         end
     end
 end
