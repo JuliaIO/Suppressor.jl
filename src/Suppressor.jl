f2362719f63b9bd6c17ef6491db1cfa9e26b57eb
Juergen Fuhrmann <juergen.fuhrmann@wias-berlin.de>
2020-09-10 16:15:41 +0200

    Add capture macro for both out and err.

--- src/Suppressor.jl
+++ src/Suppressor.jl
@@ -3,7 +3,7 @@ __precompile__()
 module Suppressor
 
 export @suppress, @suppress_out, @suppress_err
-export @capture_out, @capture_err
+export @capture, @capture_out, @capture_err
 export @color_output
 
 """
@@ -181,6 +181,59 @@ macro capture_err(block)
         end
     end
 end
+
+"""
+    @capture expr
+
+Capture the `output` and `stderr` streams for the given expression,
+return a tuple of stdout and stderr.
+"""
+macro capture(block)
+    quote
+        if ccall(:jl_generating_output, Cint, ()) == 0
+            original_stdout = stdout
+            out_rd, out_wr = redirect_stdout()
+            out_reader = @async read(out_rd, String)
+
+            original_stderr = stderr
+            err_rd, err_wr = redirect_stderr()
+            err_reader = @async read(err_rd, String)
+
+
+            
+            # approach adapted from https://github.com/JuliaLang/IJulia.jl/pull/667/files
+            logstate = Base.CoreLogging._global_logstate
+            logger = logstate.logger
+            if logger.stream == original_stderr
+                new_logstate = Base.CoreLogging.LogState(typeof(logger)(err_wr, logger.min_level))
+                Core.eval(Base.CoreLogging, Expr(:(=), :(_global_logstate), new_logstate))
+            end
+        end
+
+        try
+            $(esc(block))
+        finally
+            if ccall(:jl_generating_output, Cint, ()) == 0
+                redirect_stdout(original_stdout)
+                close(out_wr)
+
+                redirect_stderr(original_stderr)
+                close(err_wr)
+
+                if logger.stream == stderr
+                    Core.eval(Base.CoreLogging, Expr(:(=), :(_global_logstate), logstate))
+                end
+            end
+        end
+
+        if ccall(:jl_generating_output, Cint, ()) == 0
+            (fetch(out_reader),fetch(err_reader))
+        else
+            ("","")
+        end
+    end
+end
+
 
 """
     @color_output enabled::Bool expr
